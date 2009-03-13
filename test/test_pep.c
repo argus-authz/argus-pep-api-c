6910cd82466ffc347e5785588f59c47531ce9108
Valery Tschopp <valery.tschopp@switch.ch>
2009-03-13 12:27:54 +0000

    use the new xacml.h header

--- test/test_pep.c
+++ test/test_pep.c
@@ -94,84 +94,84 @@ static const char * decision_str(int decision) {
 /*
  * Dumps a PEP request.
  */
-static int dump_request(pep_request_t ** request) {
-	pep_request_t * request2= *request;
+static int dump_request(xacml_request_t ** request) {
+	xacml_request_t * request2= *request;
 	if (request == NULL) {
 		error("dump_request: request is NULL");
 		return 1;
 	}
-	size_t subjects_l= pep_request_subjects_length(request2);
+	size_t subjects_l= xacml_request_subjects_length(request2);
 	info("request: %d subjects", (int)subjects_l);
 	int i= 0;
 	for (i= 0; i<subjects_l; i++) {
-		pep_subject_t * subject= pep_request_getsubject(request2,i);
-		info("request.subject[%d].category= %s", i, pep_subject_getcategory(subject));
-		size_t attrs_l= pep_subject_attributes_length(subject);
+		xacml_subject_t * subject= xacml_request_getsubject(request2,i);
+		info("request.subject[%d].category= %s", i, xacml_subject_getcategory(subject));
+		size_t attrs_l= xacml_subject_attributes_length(subject);
 		info("request.subject[%d]: %d attributes", i, (int)attrs_l);
 		int j= 0;
 		for(j= 0; j<attrs_l; j++) {
-			pep_attribute_t * attr= pep_subject_getattribute(subject,j);
-			info("request.subject[%d].attribute[%d].id= %s", i,j,pep_attribute_getid(attr));
-			info("request.subject[%d].attribute[%d].datatype= %s", i,j,pep_attribute_getdatatype(attr));
-			info("request.subject[%d].attribute[%d].issuer= %s", i,j,pep_attribute_getissuer(attr));
-			size_t values_l= pep_attribute_values_length(attr);
+			xacml_attribute_t * attr= xacml_subject_getattribute(subject,j);
+			info("request.subject[%d].attribute[%d].id= %s", i,j,xacml_attribute_getid(attr));
+			info("request.subject[%d].attribute[%d].datatype= %s", i,j,xacml_attribute_getdatatype(attr));
+			info("request.subject[%d].attribute[%d].issuer= %s", i,j,xacml_attribute_getissuer(attr));
+			size_t values_l= xacml_attribute_values_length(attr);
 			info("request.subject[%d].attribute[%d]: %d values", i,j,(int)values_l);
 			int k= 0;
 			for (k= 0; k<values_l; k++) {
-				info("request.subject[%d].attribute[%d].value[%d]= %s", i,j,k,pep_attribute_getvalue(attr,k));
+				info("request.subject[%d].attribute[%d].value[%d]= %s", i,j,k,xacml_attribute_getvalue(attr,k));
 			}
 		}
 	}
-	size_t resources_l= pep_request_resources_length(request2);
+	size_t resources_l= xacml_request_resources_length(request2);
 	info("request: %d resources", (int)resources_l);
 	for (i= 0; i<resources_l; i++) {
-		pep_resource_t * resource= pep_request_getresource(request2,i);
-		info("request.resource[%d].content= %s", i, pep_resource_getcontent(resource));
-		size_t attrs_l= pep_resource_attributes_length(resource);
+		xacml_resource_t * resource= xacml_request_getresource(request2,i);
+		info("request.resource[%d].content= %s", i, xacml_resource_getcontent(resource));
+		size_t attrs_l= xacml_resource_attributes_length(resource);
 		info("request.resource[%d]: %d attributes", i, (int)attrs_l);
 		int j= 0;
 		for(j= 0; j<attrs_l; j++) {
-			pep_attribute_t * attr= pep_resource_getattribute(resource,j);
-			info("request.resource[%d].attribute[%d].id= %s", i,j,pep_attribute_getid(attr));
-			info("request.resource[%d].attribute[%d].datatype= %s", i,j,pep_attribute_getdatatype(attr));
-			info("request.resource[%d].attribute[%d].issuer= %s", i,j,pep_attribute_getissuer(attr));
-			size_t values_l= pep_attribute_values_length(attr);
+			xacml_attribute_t * attr= xacml_resource_getattribute(resource,j);
+			info("request.resource[%d].attribute[%d].id= %s", i,j,xacml_attribute_getid(attr));
+			info("request.resource[%d].attribute[%d].datatype= %s", i,j,xacml_attribute_getdatatype(attr));
+			info("request.resource[%d].attribute[%d].issuer= %s", i,j,xacml_attribute_getissuer(attr));
+			size_t values_l= xacml_attribute_values_length(attr);
 			info("request.resource[%d].attribute[%d]: %d values", i,j,(int)values_l);
 			int k= 0;
 			for (k= 0; k<values_l; k++) {
-				info("request.resource[%d].attribute[%d].value[%d]= %s", i,j,k,pep_attribute_getvalue(attr,k));
+				info("request.resource[%d].attribute[%d].value[%d]= %s", i,j,k,xacml_attribute_getvalue(attr,k));
 			}
 		}
 	}
-	pep_action_t * action2= pep_request_getaction(request2);
-	size_t attrs_l= pep_action_attributes_length(action2);
+	xacml_action_t * action2= xacml_request_getaction(request2);
+	size_t attrs_l= xacml_action_attributes_length(action2);
 	info("request.action: %d attributes",(int)attrs_l);
 	int j= 0;
 	for (j= 0; j<attrs_l; j++) {
-		pep_attribute_t * attr= pep_action_getattribute(action2,j);
-		info("request.action.attribute[%d].id= %s", j,pep_attribute_getid(attr));
-		info("request.action.attribute[%d].datatype= %s", j,pep_attribute_getdatatype(attr));
-		info("request.action.attribute[%d].issuer= %s", j,pep_attribute_getissuer(attr));
-		size_t values_l= pep_attribute_values_length(attr);
+		xacml_attribute_t * attr= xacml_action_getattribute(action2,j);
+		info("request.action.attribute[%d].id= %s", j,xacml_attribute_getid(attr));
+		info("request.action.attribute[%d].datatype= %s", j,xacml_attribute_getdatatype(attr));
+		info("request.action.attribute[%d].issuer= %s", j,xacml_attribute_getissuer(attr));
+		size_t values_l= xacml_attribute_values_length(attr);
 		info("request.action.attribute[%d]: %d values", j,(int)values_l);
 		int k= 0;
 		for (k= 0; k<values_l; k++) {
-			info("request.action.attribute[%d].value[%d]= %s",j,k,pep_attribute_getvalue(attr,k));
+			info("request.action.attribute[%d].value[%d]= %s",j,k,xacml_attribute_getvalue(attr,k));
 		}
 	}
-	pep_environment_t * env2= pep_request_getenvironment(request2);
-	attrs_l= pep_environment_attributes_length(env2);
+	xacml_environment_t * env2= xacml_request_getenvironment(request2);
+	attrs_l= xacml_environment_attributes_length(env2);
 	info("request.environment: %d attributes",(int)attrs_l);
 	for (j= 0; j<attrs_l; j++) {
-		pep_attribute_t * attr= pep_environment_getattribute(env2,j);
-		info("request.environment.attribute[%d].id= %s", j,pep_attribute_getid(attr));
-		info("request.environment.attribute[%d].datatype= %s", j,pep_attribute_getdatatype(attr));
-		info("request.environment.attribute[%d].issuer= %s", j,pep_attribute_getissuer(attr));
-		size_t values_l= pep_attribute_values_length(attr);
+		xacml_attribute_t * attr= xacml_environment_getattribute(env2,j);
+		info("request.environment.attribute[%d].id= %s", j,xacml_attribute_getid(attr));
+		info("request.environment.attribute[%d].datatype= %s", j,xacml_attribute_getdatatype(attr));
+		info("request.environment.attribute[%d].issuer= %s", j,xacml_attribute_getissuer(attr));
+		size_t values_l= xacml_attribute_values_length(attr);
 		info("request.environment.attribute[%d]: %d values", j,(int)values_l);
 		int k= 0;
 		for (k= 0; k<values_l; k++) {
-			info("request.environment.attribute[%d].value[%d]= %s",j,k,pep_attribute_getvalue(attr,k));
+			info("request.environment.attribute[%d].value[%d]= %s",j,k,xacml_attribute_getvalue(attr,k));
 		}
 	}
 	return 0;
@@ -180,44 +180,44 @@ static int dump_request(pep_request_t ** request) {
 /*
  * Dumps a PEP response.
  */
-static int dump_response(pep_response_t ** response_ptr) {
-	pep_response_t * response= *response_ptr;
+static int dump_response(xacml_response_t ** response_ptr) {
+	xacml_response_t * response= *response_ptr;
 	if (response == NULL) {
 		error("dump_response: response is NULL");
 		return 1;
 	}
-	size_t results_l= pep_response_results_length(response);
+	size_t results_l= xacml_response_results_length(response);
 	info("response: %d results", (int)results_l);
 	int i= 0;
 	for(i= 0; i<results_l; i++) {
-		pep_result_t * result= pep_response_getresult(response,i);
-		info("response.result[%d].decision= %s", i, decision_str(pep_result_getdecision(result)));
-		info("response.result[%d].resourceid= %s", i, pep_result_getresourceid(result));
-		pep_status_t * status= pep_result_getstatus(result);
-		info("response.result[%d].status.message= %s", i, pep_status_getmessage(status));
-		pep_status_code_t * statuscode= pep_status_getcode(status);
-		info("response.result[%d].status.code.value= %s", i, pep_status_code_getvalue(statuscode));
-		pep_status_code_t * subcode= pep_status_code_getsubcode(statuscode);
+		xacml_result_t * result= xacml_response_getresult(response,i);
+		info("response.result[%d].decision= %s", i, decision_str(xacml_result_getdecision(result)));
+		info("response.result[%d].resourceid= %s", i, xacml_result_getresourceid(result));
+		xacml_status_t * status= xacml_result_getstatus(result);
+		info("response.result[%d].status.message= %s", i, xacml_status_getmessage(status));
+		xacml_statuscode_t * statuscode= xacml_status_getcode(status);
+		info("response.result[%d].status.code.value= %s", i, xacml_statuscode_getvalue(statuscode));
+		xacml_statuscode_t * subcode= xacml_statuscode_getsubcode(statuscode);
 		if (subcode != NULL) {
-			info("response.result[%d].status.code.subcode.value= %s", i, pep_status_code_getvalue(subcode));
+			info("response.result[%d].status.code.subcode.value= %s", i, xacml_statuscode_getvalue(subcode));
 		}
-		size_t obligations_l= pep_result_obligations_length(result);
+		size_t obligations_l= xacml_result_obligations_length(result);
 		info("response.result[%d]: %d obligations", i, (int)obligations_l);
 		int j=0;
 		for(j= 0; j<obligations_l; j++) {
-			pep_obligation_t * obligation= pep_result_getobligation(result,j);
-			info("response.result[%d].obligation[%d].id= %s",i,j, pep_obligation_getid(obligation));
-			info("response.result[%d].obligation[%d].fulfillOn= %s",i,j, decision_str(pep_obligation_getfulfillon(obligation)));
-			size_t attrs_l= pep_obligation_attributeassignments_length(obligation);
+			xacml_obligation_t * obligation= xacml_result_getobligation(result,j);
+			info("response.result[%d].obligation[%d].id= %s",i,j, xacml_obligation_getid(obligation));
+			info("response.result[%d].obligation[%d].fulfillOn= %s",i,j, decision_str(xacml_obligation_getfulfillon(obligation)));
+			size_t attrs_l= xacml_obligation_attributeassignments_length(obligation);
 			info("response.result[%d].obligation[%d]: %d attribute assignments",i,j,(int)attrs_l);
 			int k= 0;
 			for (k= 0; k<attrs_l; k++) {
-				pep_attribute_assignment_t * attr= pep_obligation_getattributeassignment(obligation,k);
-				info("response.result[%d].obligation[%d].attributeassignment[%d].id= %s",i,j,k,pep_attribute_assignment_getid(attr));
-				size_t values_l= pep_attribute_assignment_values_length(attr);
+				xacml_attributeassignment_t * attr= xacml_obligation_getattributeassignment(obligation,k);
+				info("response.result[%d].obligation[%d].attributeassignment[%d].id= %s",i,j,k,xacml_attributeassignment_getid(attr));
+				size_t values_l= xacml_attributeassignment_values_length(attr);
 				int l= 0;
 				for (l= 0; l<values_l; l++) {
-					info("response.result[%d].obligation[%d].attributeassignment[%d].value[%d]= %s",i,j,k,l,pep_attribute_assignment_getvalue(attr,l));
+					info("response.result[%d].obligation[%d].attributeassignment[%d].value[%d]= %s",i,j,k,l,xacml_attributeassignment_getvalue(attr,l));
 				}
 			}
 		}
@@ -236,7 +236,7 @@ static int pip_init(void) {
 /*
  * PIP process function called by the PEP client before submitting to PEPd in pep_authorize(...)
  */
-static int pip_process(pep_request_t ** request) {
+static int pip_process(xacml_request_t ** request) {
 	debug("pip_process(request) called...");
 	info("PIP dump request:");
 	return dump_request(request);
@@ -300,7 +300,7 @@ static int oh_init(void) {
 /*
  * OH process(request,response) called by the PEP client after receiving the PEPd reponse in pep_authorize(...)
  */
-static int oh_process(pep_request_t ** request, pep_response_t ** response) {
+static int oh_process(xacml_request_t ** request, xacml_response_t ** response) {
 	debug("oh_process(request,response) called...");
 	info("OH dump response:");
 	return dump_response(response);
@@ -407,60 +407,63 @@ int main(int argc, char **argv) {
 
 	// create a PEP request
 	info("create PEP request...");
-	pep_request_t * request= pep_request_create();
+	xacml_request_t * request= xacml_request_create();
 	assert(request);
 	info("add PEP subject(proxy)...");
-	pep_subject_t * subject= pep_subject_create();
+	xacml_subject_t * subject= xacml_subject_create();
 	assert(subject);
-	pep_attribute_t * proxy= pep_attribute_create("x-urn:authz:subject:proxy");
+	xacml_attribute_t * proxy= xacml_attribute_create("x-urn:authz:subject:proxy");
 	assert(proxy);
-	pep_attribute_addvalue(proxy,"PEM_ENCODE_PROXY_CONTENT");
-	pep_subject_addattribute(subject,proxy);
-	pep_request_addsubject(request,subject);
+	xacml_attribute_addvalue(proxy,"PEM_ENCODE_PROXY_CONTENT");
+	xacml_subject_addattribute(subject,proxy);
+	xacml_request_addsubject(request,subject);
 	info("add PEP resource(jdl,name)...");
-	pep_resource_t * resource= pep_resource_create();
+	xacml_resource_t * resource= xacml_resource_create();
 	assert(resource);
-	pep_attribute_t * jdl= pep_attribute_create("x-urn:authz:resource:jdl");
+	xacml_attribute_t * jdl= xacml_attribute_create("x-urn:authz:resource:jdl");
 	assert(jdl);
-	pep_attribute_addvalue(jdl,"JDL_FILE_CONTENT");
-	pep_resource_addattribute(resource,jdl);
-	pep_attribute_t * name= pep_attribute_create("x-urn:authz:resource:name");
+	xacml_attribute_addvalue(jdl,"JDL_FILE_CONTENT");
+	xacml_resource_addattribute(resource,jdl);
+	xacml_attribute_t * name= xacml_attribute_create("x-urn:authz:resource:name");
 	assert(name);
-	pep_attribute_addvalue(name,"Val\xC3\xA9ry");
-	pep_resource_addattribute(resource,name);
-	pep_request_addresource(request,resource);
+	xacml_attribute_addvalue(name,"Val\xC3\xA9ry");
+	xacml_resource_addattribute(resource,name);
+	xacml_request_addresource(request,resource);
 	info("set PEP action(command)...");
-	pep_action_t * action= pep_action_create();
+	xacml_action_t * action= xacml_action_create();
 	assert(action);
-	pep_attribute_t * command= pep_attribute_create("x-urn:authz:action:command");
+	xacml_attribute_t * command= xacml_attribute_create("x-urn:authz:action:command");
 	assert(command);
-	pep_attribute_addvalue(command,"/usr/bin/echo");
-	pep_action_addattribute(action,command);
-	pep_request_setaction(request,action);
+	xacml_attribute_addvalue(command,"/usr/bin/echo");
+	xacml_action_addattribute(action,command);
+	xacml_request_setaction(request,action);
 	info("set PEP environment(path)...");
-	pep_environment_t * environment= pep_environment_create();
+	xacml_environment_t * environment= xacml_environment_create();
 	assert(environment);
-	pep_attribute_t * path= pep_attribute_create("x-urn:authz:env:path");
+	xacml_attribute_t * path= xacml_attribute_create("x-urn:authz:env:path");
 	assert(path);
-	pep_attribute_addvalue(path,"/usr/bin");
-	pep_attribute_addvalue(path,"/opt/glite/bin");
-	pep_attribute_addvalue(path,"/home/johndoe/bin");
-	pep_environment_addattribute(environment,path);
-	pep_request_setenvironment(request,environment);
+	xacml_attribute_addvalue(path,"/usr/bin");
+	xacml_attribute_addvalue(path,"/opt/glite/bin");
+	xacml_attribute_addvalue(path,"/home/johndoe/bin");
+	xacml_environment_addattribute(environment,path);
+	xacml_request_setenvironment(request,environment);
 
+	// add many PEPd endpoints for failover testing
+	info("set PEPd endpoint: %s", url);
+	pep_rc= pep_setoption(PEP_OPTION_ENDPOINT_URL, url);
 	info("set PEPd endpoint: %s", "http://www.google.com");
 	pep_rc= pep_setoption(PEP_OPTION_ENDPOINT_URL, "http://www.google.com");
 	info("set PEPd endpoint: %s", "http://localhost:8080/PEPd/authz?s7");
 	pep_rc= pep_setoption(PEP_OPTION_ENDPOINT_URL, "http://localhost:8080/PEPd/authz?s7");
 	info("set PEPd endpoint: %s", "http://nasjflkasdjflj.com");
 	pep_rc= pep_setoption(PEP_OPTION_ENDPOINT_URL, "http://nasjflkasdjflj.com");
-	info("set PEPd endpoint: %s", "http://localhost:8080/PEPd/authz?s8");
-	pep_rc= pep_setoption(PEP_OPTION_ENDPOINT_URL, "http://localhost:8080/PEPd/authz?s8");
+	info("set PEPd endpoint: %s", "http://localhost:8080/PEPd/authz?s3");
+	pep_rc= pep_setoption(PEP_OPTION_ENDPOINT_URL, "http://localhost:8080/PEPd/authz?s3");
+	info("set PEPd endpoint: %s", "http://hestia.switch.ch/PEPd/authz?s8");
+	pep_rc= pep_setoption(PEP_OPTION_ENDPOINT_URL, "http://hestia.switch.ch/PEPd/authz?s8");
 	info("set PEPd endpoint: %s", "http://localhost:8080"); // respond OK 200
 	pep_rc= pep_setoption(PEP_OPTION_ENDPOINT_URL, "http://localhost:8080");
 	// send authz request and process
-	info("set PEPd endpoint: %s", url);
-	pep_rc= pep_setoption(PEP_OPTION_ENDPOINT_URL, url);
 	if (pep_rc != PEP_OK) {
 		error("test_pep: pep_setoption(PEP_OPTION_ENDPOINT_URL,%s) failed: %s",url,pep_strerror(pep_rc));
 		pep_destroy();
@@ -468,12 +471,12 @@ int main(int argc, char **argv) {
 	}
 
 	info("send authorization request to PEPd");
-	pep_response_t * response= NULL;
+	xacml_response_t * response= NULL;
 	pep_rc= pep_authorize(&request,&response);
 	if (pep_rc != PEP_OK) {
 		error("test_pep: pep_authorize(request,response) failed: %s",pep_strerror(pep_rc));
-		pep_request_delete(request);
-		pep_response_delete(response);
+		xacml_request_delete(request);
+		xacml_response_delete(response);
 		pep_destroy();
 		pip_delete(pip);
 		oh_delete(oh);
@@ -481,8 +484,8 @@ int main(int argc, char **argv) {
 	}
 
 	info("delete PEP request and response...");
-	pep_request_delete(request);
-	pep_response_delete(response);
+	xacml_request_delete(request);
+	xacml_response_delete(response);
 
 	info("destroy PEP...");
 	pep_rc= pep_destroy();
