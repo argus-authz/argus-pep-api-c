1797481ee3cda7e597d0cdd397ccc8709b7695bb
Valery Tschopp <valery.tschopp@switch.ch>
2009-03-31 08:21:42 +0000

    removed aclocal.m4 from CVS autotools generate aclocal.m4 configure.ac doesn't check for mkdir_p

--- autotools.sh
+++ autotools.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 #set -x
 #
-# $Id: autotools.sh,v 1.3 2009/03/31 08:08:39 vtschopp Exp $
+# $Id: autotools.sh,v 1.4 2009/03/31 08:21:42 vtschopp Exp $
 #
 
 echo "Bootstrapping autotools..."
 
-#echo "aclocal..."
-#aclocal -I project
+echo "aclocal..."
+aclocal -I project
 echo "libtoolize..."
 libtoolize --force
 echo "autoheader..."
